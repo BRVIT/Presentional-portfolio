e71021fc1722baeb1d47f2ffa0cf8111c7c8c679
Bryan Richard Vandas <b.richard@enbw.com>
2026-03-05 09:05:59 +0100

    Style changes 2

--- src/components/PortfolioSection.jsx
+++ src/components/PortfolioSection.jsx
@@ -11,68 +11,47 @@ export default function PortfolioSection() {
         <div className="card card--1 reveal-box reveal">
           <div className="card__content">
             <div className="card__front card__front--1 card__block">
-              <i className="ph-game-controller card__icon"></i>
-              <h3 className="card__title">Api-NHL</h3>
-              <p className="card__text">
-                Tato aplikácia sa sústredí na prácu s api, teda na získavanie
-                jednotlivých informácii za pomoci Axiosu, kde následne
-                jednotlivé informácie sú vyrenderované na stránke. Môžete si
-                vyhľadať všetky tímy NHL, kde všetky tímy sú vypísane celkovo a
-                rozdelene do konferencii, taktiež sa môžete o nich dozvedieť
-                základne informácie a ich postavenie v tabuľke.
+              <i className="ph-chart-line-up card__icon"></i>
+              <h3 className="card__title">Fundway</h3>
+              <p className="card__text card__text--fundway">
+                Fundway je full-stack SaaS app pre fund manazerov a investorov.
+                Kalkulacka vypocita realne naklady fondu podla jurisdikcie,
+                velkosti, poctu investorov a strategie. Platforma odporuci
+                providerov v 80+ jurisdikciach a po platbe cez Stripe
+                automaticky pripravi personalizovany PDF report.
               </p>
             </div>
             <div className="card__back card__block">
-              <ul className="card__list">
+              <ul className="card__list card__list--compact">
                 <li>
-                  <h4 className="card__tools-heading">Použité technológie</h4>
+                  <h4 className="card__tools-heading">Core stack + highlights</h4>
                 </li>
-                <li className="card__list-item">
-                  <img
-                    src="icons/html5.svg"
-                    alt="html icon"
-                    className="card__icon--back"
-                  />
-                  <p>HTML</p>
+                <li className="card__list-item card__list-item--text">
+                  <i className="ph-check-circle card__list-icon"></i>
+                  <p>Next.js 16, React 19, TypeScript 5</p>
                 </li>
-                <li className="card__list-item">
-                  <img
-                    src="icons/css3.svg"
-                    alt="css icon"
-                    className="card__icon--back"
-                  />
-                  <p>CSS</p>
+                <li className="card__list-item card__list-item--text">
+                  <i className="ph-check-circle card__list-icon"></i>
+                  <p>Tailwind, Radix UI, Framer Motion, GSAP</p>
                 </li>
-                <li className="card__list-item">
-                  <img
-                    src="icons/javascript.svg"
-                    alt="javascript icon"
-                    className="card__icon--back"
-                  />
-                  <p>Javascript</p>
+                <li className="card__list-item card__list-item--text">
+                  <i className="ph-check-circle card__list-icon"></i>
+                  <p>Stripe PaymentIntents + webhook flow</p>
                 </li>
-                <li className="card__list-item">
-                  <img
-                    src="icons/axios.svg"
-                    alt="axios icon"
-                    className="card__icon--back"
-                  />
-                  <p>Axios</p>
+                <li className="card__list-item card__list-item--text">
+                  <i className="ph-check-circle card__list-icon"></i>
+                  <p>Serverless API Routes, Prisma, PostgreSQL</p>
                 </li>
-                <li className="card__list-item">
-                  <img
-                    src="icons/vite.svg"
-                    alt="vite icon"
-                    className="card__icon--back"
-                  />
-                  <p>Vite</p>
+                <li className="card__list-item card__list-item--text">
+                  <i className="ph-check-circle card__list-icon"></i>
+                  <p>PDFKit, Resend, WordPress REST + GraphQL</p>
                 </li>
               </ul>
               <a
-                href="https://nhl-api-portfolio.netlify.app"
+                href="https://goFundway.com"
                 className="btn btn--card btn--animated"
               >
-                Vyskúšaj to
+                Pozri projekt
               </a>
             </div>
           </div>
